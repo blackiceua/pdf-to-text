aba32e1b66fda8e8f6fdd928d03e4f637d7ec1b4
Oleksandr Chernyi <o.o.chernyi@gmail.com>
2017-12-04 13:42:46 +0200

    Added additional methods (toArray, json). Added a method for parameterizing the execution of the utility from an array of options(setOptions).

--- src/Pdf.php
+++ src/Pdf.php
@@ -17,6 +17,17 @@ class Pdf
         $this->binPath = $binPath ?? '/usr/bin/pdftotext';
     }
 
+    public function setOptions(array $options) : Pdf
+    {
+        if (!empty($options)) {
+            foreach ($options as $option) {
+                $this->binPath .= ' ' . escapeshellarg(trim($option));
+            }
+        }
+
+        return $this;
+    }
+
     public function setPdf(string $pdf) : Pdf
     {
         if (!file_exists($pdf)) {
@@ -40,9 +51,33 @@ class Pdf
         return trim($process->getOutput(), " \t\n\r\0\x0B\x0C");
     }
 
-    public static function getText(string $pdf, string $binPath = null) : string
+    public function toArray() : array
+    {
+      $process = new Process("{$this->binPath} " . escapeshellarg($this->pdf) . " -");
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new CouldNotExtractText($process);
+        }
+
+        $pages = explode("\f", $process->getOutput());
+        foreach($pages as $key => $page)
+        {
+            $pages[$key] = explode("\n", $page);
+        }
+
+        return $pages;
+   }
+
+   public function json() : string
+   {
+        return json_encode($this->toArray());
+   }
+
+    public static function getText(string $pdf, string $binPath = null, $options = []) : string
     {
         return (new static($binPath))
+            ->setOptions($options)
             ->setPdf($pdf)
             ->text();
     }
